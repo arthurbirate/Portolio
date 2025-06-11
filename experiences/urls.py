bfdfc297b4246161781304d1485fb6d209f5d89e
Birate Arthur <biratearthur@Birates-MacBook-Air.local>
2025-06-11 13:18:58 +0200

    v1.19

--- experiences/urls.py
+++ experiences/urls.py
@@ -5,4 +5,5 @@ from .views import add_internship
 urlpatterns = [
     path('', internship_view, name='internship'),
       path('add/', add_internship, name='add_internship')
+       path('edit/<int:pk>/', views.edit_internship, name='edit_internship'),
 ]
--- experiences/views.py
+++ experiences/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from .forms import InternshipForm
 
 # Create your views here.
@@ -15,7 +15,20 @@ def add_internship(request):
         form = InternshipForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('internship')  # Change to your success page or internship list page
+            return redirect('internship')  
     else:
         form = InternshipForm()
     return render(request, 'experiences/add_internship.html', {'form': form})
+
+
+
+def edit_internship(request, pk):
+    internship = get_object_or_404(Internship, pk=pk)
+    if request.method == 'POST':
+        form = InternshipForm(request.POST, instance=internship)
+        if form.is_valid():
+            form.save()
+            return redirect('internship')  
+    else:
+        form = InternshipForm(instance=internship)
+    return render(request, 'experiences/edit_internship.html', {'form': form, 'internship': internship})
