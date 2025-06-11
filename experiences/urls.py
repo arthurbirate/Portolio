101b0ccb9acbfad16c022943c7c9b87625b7c9cf
Birate Arthur <biratearthur@Birates-MacBook-Air.local>
2025-06-11 13:23:48 +0200

    v1.20

--- experiences/urls.py
+++ experiences/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import internship_view
-from .views import add_internship
+from .views import add_internship, edit_internship
 
 urlpatterns = [
     path('', internship_view, name='internship'),
-      path('add/', add_internship, name='add_internship')
-       path('edit/<int:pk>/', views.edit_internship, name='edit_internship'),
+      path('add/', add_internship, name='add_internship'),
+       path('edit/<int:pk>/', edit_internship, name='edit_internship'),
 ]
